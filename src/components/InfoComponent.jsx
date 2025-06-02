a9a2f082c516b3aeb3b328fbf2e3ebd7bd6200d2
Eddie <eddie11910@outlook.com>
2025-06-01 20:36:28 -0700

    Implemented a part of the infoFields map method

--- src/components/InfoComponent.jsx
+++ src/components/InfoComponent.jsx
@@ -1,3 +1,13 @@
-function InfoComponent({ infoFields }) {}
+function InfoComponent({ infoFields }) {
+  return (
+    <section className="d-flex flex-column">
+      {infoFields.map((item, i) => {
+        <label htmlFor={item.name} key={i}>
+          {item.text}
+        </label>;
+      })}
+    </section>
+  );
+}
 
 return InfoComponent;
