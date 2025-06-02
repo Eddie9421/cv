0ab21dcb0027b2d0cb3dcce6391a2d04536cc04a
Eddie <eddie11910@outlook.com>
2025-06-01 17:35:30 -0700

    Finished basic HTML of general info component

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,14 +1,14 @@
 function GeneralInfo() {
   return (
-    <div className="d-flex">
+    <div className="d-flex flex-column">
       <label htmlFor="name">
         Name: <input type="text" id="name" />
       </label>
       <label htmlFor="email">
         Email: <input type="email" id="email" />
       </label>
-      <label htmlFor="">
-        <input type="text" />
+      <label htmlFor="phone-number">
+        Phone Number: <input type="tel" id="phone-number" />
       </label>
     </div>
   );
