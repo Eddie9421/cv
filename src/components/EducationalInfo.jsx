c69e7a7bd29ae82b8fcf83781cb90b2e593fc86a
Eddie <eddie11910@outlook.com>
2025-06-01 17:47:29 -0700

    Copied general HTML format from the general info component

--- src/components/EducationalInfo.jsx
+++ src/components/EducationalInfo.jsx
@@ -1,3 +1,17 @@
-function EducationalInfo() {}
+function EducationalInfo() {
+  return (
+    <section className="d-flex flex-column">
+      <label htmlFor="name">
+        Name: <input type="text" id="name" />
+      </label>
+      <label htmlFor="email">
+        Email: <input type="email" id="email" />
+      </label>
+      <label htmlFor="phone-number">
+        Phone Number: <input type="tel" id="phone-number" />
+      </label>
+    </section>
+  );
+}
 
 export default EducationalInfo;
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,6 +1,6 @@
 function GeneralInfo() {
   return (
-    <div className="d-flex flex-column">
+    <section className="d-flex flex-column">
       <label htmlFor="name">
         Name: <input type="text" id="name" />
       </label>
@@ -10,7 +10,7 @@ function GeneralInfo() {
       <label htmlFor="phone-number">
         Phone Number: <input type="tel" id="phone-number" />
       </label>
-    </div>
+    </section>
   );
 }
 
