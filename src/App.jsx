37e6332e7763d7d48280bb44f744bc3983d644d8
Eddie <eddie11910@outlook.com>
2025-06-01 19:55:44 -0700

    Revert "Added component to App.jsx"
    This reverts commit b26592722cfede834199bbba75fe0bf5d4b1df42.
    
    Reverted adding a new component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import GeneralInfo from "./components/GeneralInfo";
-import EducationalInfo from "./components/EducationalInfo";
 
 function App() {
   return (
     <>
       <h1>CV Creator</h1>
       <GeneralInfo />
-      <EducationalInfo />
     </>
   );
 }
