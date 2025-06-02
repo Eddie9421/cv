9c052850ff5f345d339d8d5f3e6bb40335748f6c
Eddie <eddie11910@outlook.com>
2025-06-01 17:21:14 -0700

    Made the general format for the component

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,3 +1,18 @@
-function GeneralInfo() {}
+function GeneralInfo() {
+  return (
+    <>
+      <label htmlFor="name">
+        What's your name?
+        <input type="text" id="name" />
+      </label>
+      <label htmlFor="">
+        <input type="text" />
+      </label>
+      <label htmlFor="">
+        <input type="text" />
+      </label>
+    </>
+  );
+}
 
 export default GeneralInfo;
