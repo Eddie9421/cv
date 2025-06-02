c5b2c20c0e0ec706cf7657132803b1911710fbae
Eddie <eddie11910@outlook.com>
2025-06-01 20:52:59 -0700

    Fixed map method not returning anything and type attribute not getting the correct props

--- src/components/InfoComponent.jsx
+++ src/components/InfoComponent.jsx
@@ -1,14 +1,14 @@
 function InfoComponent({ infoFields }) {
   return (
     <section className="d-flex flex-column">
-      {infoFields.map((item, i) => {
+      {infoFields.map((item, i) => (
         <label htmlFor={item.name} key={i}>
           {item.text}
-          <input type={item.name} id={item.name} />
-        </label>;
-      })}
+          <input type={item.type} id={item.name} />
+        </label>
+      ))}
     </section>
   );
 }
 
-return InfoComponent;
+export default InfoComponent;
