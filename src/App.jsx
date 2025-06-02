75d5fdf31c061b391a864ac5696bfa6bac39b565
Eddie <eddie11910@outlook.com>
2025-06-01 20:10:24 -0700

    Added educational info component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import GeneralInfo from "./components/GeneralInfo";
+import EducationalInfo from "./components/EducationalInfo";
 
 function App() {
   return (
     <>
       <h1>CV Creator</h1>
       <GeneralInfo />
+      <EducationalInfo />
     </>
   );
 }
